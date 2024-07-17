1d8149e77274b96661d88758bd19d52ec87654ce
vlambo3 <contadora.godoy.vanina@gmail.com>
2024-07-17 16:24:23 -0300

    Modified the constructor of the CheckEmail

--- src/CheckEmail.java
+++ src/CheckEmail.java
@@ -5,33 +5,28 @@ La clase chequeará el manejo de mail dependiendo de su destino o tema.
 public class CheckEmail {
 
     //Representa el primer manejador en la cadena de responsabilidad.
-    private Handler chain;
+    private Handler handler;
 
-    //Método para crear la cadena de responsabilidad
-    private Handler createChain() {
-        // Se crean instancias de cada manejador específico.
-        Handler managerHandler = new ManagerHandler();
+    // Constructor que inicializa la cadena de responsabilidad.
+    public CheckEmail() {
+
+        //Se inicia la cadena de responsablidad
+        handler = new ManagerHandler();
+
+        //Construímos la cadena
         Handler comercialHandler = new ComercialHandler();
         Handler technicalHandler = new TechnicalHandler();
         Handler spamHandler = new SpamHandler();
 
-        // Se establecen las relaciones de encadenamiento entre los manejadores.
-        managerHandler.setNextHandler(comercialHandler);
+        //Unimos la cadena
+        handler.setNextHandler(comercialHandler);
         comercialHandler.setNextHandler(technicalHandler);
         technicalHandler.setNextHandler(spamHandler);
-
-        // Se devuelve el primer manejador de la cadena.
-        return managerHandler;
-    }
-
-    // Constructor que inicializa la cadena de responsabilidad.
-    public CheckEmail() {
-        this.chain = createChain();
     }
 
     // Método público para iniciar el proceso de manejo de un correo electrónico.
     public String checkEmail(Email email) {
         // Se inicia el procesamiento del correo electrónico a través de la cadena de responsabilidad.
-        return chain.handleRequest(email);
+        return handler.handleRequest(email);
     }
 }
